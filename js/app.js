b2df4e7280627b7a014a29c6fe4b7e7163ee1406
Flor <florenciadaddin@gmail.com>
2026-09-19 19:37:19 -0300

    v12

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 console.log("Portfolio Arantza cargado correctamente");
+/* =========================
+   SCROLL REVEAL
+========================= */
+
+const revealElements = document.querySelectorAll(
+  ".reveal, .reveal-left, .reveal-right"
+);
+
+const revealObserver = new IntersectionObserver(
+  (entries) => {
+
+    entries.forEach((entry) => {
+
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+
+        revealObserver.unobserve(entry.target);
+      }
+
+    });
+
+  },
+  {
+    threshold: 0.15
+  }
+);
+
+
+revealElements.forEach((element) => {
+  revealObserver.observe(element);
+});
